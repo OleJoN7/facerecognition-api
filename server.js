7eed4fb21d1d2f9bb62ff52f95fd765ff622b29e
oleg <oleb140888@gmail.com>
2019-01-11 14:49:14 +0200

    change port

--- server.js
+++ server.js
@@ -112,8 +112,8 @@ app.post('/imageurl',(req,res) => {
     .catch(err => res.status(400).json('Unable to work with Clarifai API'))
 })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`)
 })
 
 /*
